cfa092d5aed323591afe5ba4cb8abc1269a19fb3
Tobias Neef <tobnee@gmail.com>
2014-10-14 19:25:54 +0200

    Add initial usage examples and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# DValidation - Scala Domain Validation Toolkit
+A little, opinionated domain object validation toolkit on top of [scalaz](https://github.com/scalaz/scalaz)
+
+## Basic Usage
+```scala
+
+```
---
+++ src/test/scala/com/atinu/dvalidation/UsageSamples.scala
@@ -0,0 +1,58 @@
+package com.atinu.dvalidation
+
+import com.atinu.dvalidation.Validator
+import com.atinu.dvalidation.Validator._
+
+object UsageSamples extends App {
+
+  trait Classification
+  case object StringInstrument extends Classification
+  case object Keyboard extends Classification
+
+  abstract class Instrument(val classification: Classification)
+  case object BassGuitar extends Instrument(StringInstrument)
+  case object Guitar extends Instrument(StringInstrument)
+  case object Piano extends Instrument(Keyboard)
+
+  case class Musician(name: String, age: Int, instruments: Seq[Instrument])
+
+  // Ad-Hoc validation for a case class
+  val mikael = Musician("Mikael Åkerfeldt", 40, List(Guitar, BassGuitar))
+
+  val res: DValidation[Musician] = mikael.validateWith(
+    notEmpty(mikael.name) forAttribute 'name,
+    ensure(mikael.age)("error.dvalidation.legalage", 18)(_ > 18) forAttribute 'age,
+    hasElements(mikael.instruments) forAttribute 'instruments
+  )
+  // => Success(User(Mikael Åkerfeldt,40))
+
+  // Validation Templates
+  val musicianValidator: DValidator[Musician] = Validator.template[Musician] { musician =>
+    musician.validateWith(
+      notEmpty(musician.name) forAttribute 'name,
+      ensure(musician.age)(key = "error.dvalidation.legalage", args = 18)(_ > 18) forAttribute 'age,
+      hasElements(musician.instruments) forAttribute 'instruments
+    )
+  }
+  musicianValidator(mikael)
+  // => Success(User(Mikael Åkerfeldt,40))
+  musicianValidator(Musician("Martin Mendez", 17, List(BassGuitar)))
+  // => Failure(DomainError(path: /age, value: 17, msgKey: error.dvalidation.legalage, args: 18))
+
+  // Sequence Validation
+  val max = Musician("Max Mustermann", 29, List(Piano, Guitar))
+
+  val stringInstrumentValidator = Validator.template[Instrument](i =>
+    ensure(i)(key = "error.dvalidation.stringinstrument", args = i.classification)(_.classification == StringInstrument)
+  )
+
+  val a = max.validateWith(
+    notEmpty(max.name) forAttribute 'name,
+    ensure(max.age)("error.dvalidation.legalage", 18)(_ > 18) forAttribute 'age,
+    hasElements(max.instruments) forAttribute 'instruments
+  ).withValidations(
+      validSequence(max.instruments, stringInstrumentValidator) forAttribute 'instruments
+    )
+
+  // => Failure(DomainError(path: /instruments/[0], value: Piano, msgKey: error.dvalidation.stringinstrument, args: Keyboard))
+}
