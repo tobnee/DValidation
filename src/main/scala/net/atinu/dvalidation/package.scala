203fbeb24c12754f1a17ca37b56762e9f9402cec
Tobias Neef <tobnee@gmail.com>
2014-10-28 18:07:30 +0100

    extract ErrorMap class to file

--- src/main/scala/net/atinu/dvalidation/package.scala
+++ src/main/scala/net/atinu/dvalidation/package.scala
@@ -123,29 +123,6 @@ package object dvalidation {
     }
   }
 
-  trait ErrorMap[-T] extends (T => DomainError)
-
-  object ErrorMap {
-
-    private val id: PartialFunction[DomainError, DomainError] = { case e => e }
-
-    def apply[T <: DomainError](f: T => DomainError): ErrorMap[T] = new ErrorMap[T] {
-      def apply(error: T) = f(error)
-    }
-
-    def dispatch(f: PartialFunction[DomainError, DomainError]): ErrorMap[DomainError] = new ErrorMap[DomainError] {
-      def apply(error: DomainError) = f.orElse(id)(error)
-    }
-
-    def mapKey[T <: DomainError](key: String): ErrorMap[T] = new ErrorMap[T] {
-      def apply(error: T) = CustomValidationError.withKey(error, key)
-    }
-
-    implicit object DomainErrorIdentity extends ErrorMap[DomainError] {
-      def apply(in: DomainError): DomainError = in
-    }
-  }
-
   implicit def domainErrorsInstances =
     new Semigroup[DomainErrors] with Equal[DomainErrors] {
       def append(f1: DomainErrors, f2: => DomainErrors): DomainErrors = {
