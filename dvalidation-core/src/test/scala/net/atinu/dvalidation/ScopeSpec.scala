b45f039523bcd19c459f33ac3f13a2069141ee5c
Tobias Neef <tobnee@gmail.com>
2015-04-02 15:09:30 +0200

    simplify tests

--- dvalidation-core/src/test/scala/net/atinu/dvalidation/ScopeSpec.scala
+++ dvalidation-core/src/test/scala/net/atinu/dvalidation/ScopeSpec.scala
@@ -9,24 +9,23 @@ import net.atinu.dvalidation.scopes._
 class ScopeSpec extends ValidationSuite {
 
   case class CTest(a: String, b: String, c: String)
+  val emptyVal = new CTest("", "", "")
 
   test("a scoped validation can apply validations in a named scope") {
-    val value = new CTest("", "", "")
     import SymbolScope._
-    value.validateScoped('foo)(
-      inScope('foo)(notBlank(value.a) forAttribute 'a),
-      inScope('foo2)(notBlank(value.b) forAttribute 'b),
-      inScope('foo2)(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateScoped('foo)(
+      inScope('foo)(notBlank(emptyVal.a) forAttribute 'a),
+      inScope('foo2)(notBlank(emptyVal.b) forAttribute 'b),
+      inScope('foo2)(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithError(new IsEmptyStringError(Path.wrap("/a")))
   }
 
   test("a scoped validation can apply validations in a symbol named scope") {
-    val value = new CTest("", "", "")
     import SymbolScope._
-    value.validateScoped('foo2)(
-      inScope('foo)(notBlank(value.a) forAttribute 'a),
-      inScope('foo2)(notBlank(value.b) forAttribute 'b),
-      inScope('foo2)(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateScoped('foo2)(
+      inScope('foo)(notBlank(emptyVal.a) forAttribute 'a),
+      inScope('foo2)(notBlank(emptyVal.b) forAttribute 'b),
+      inScope('foo2)(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithErrors(
         new IsEmptyStringError(Path.wrap("/b")),
         new IsEmptyStringError(Path.wrap("/c"))
@@ -34,13 +33,12 @@ class ScopeSpec extends ValidationSuite {
   }
 
   test("a scoped validation can apply multible validations in one scope") {
-    val value = new CTest("", "", "")
     import SymbolScope._
-    value.validateScoped('foo2)(
-      inScope('foo)(notBlank(value.a) forAttribute 'a),
+    emptyVal.validateScoped('foo2)(
+      inScope('foo)(notBlank(emptyVal.a) forAttribute 'a),
       allInScope('foo2)(List(
-        notBlank(value.b) forAttribute 'b,
-        notBlank(value.c) forAttribute 'c))
+        notBlank(emptyVal.b) forAttribute 'b,
+        notBlank(emptyVal.c) forAttribute 'c))
     ) should beInvalidWithErrors(
         new IsEmptyStringError(Path.wrap("/b")),
         new IsEmptyStringError(Path.wrap("/c"))
@@ -48,24 +46,22 @@ class ScopeSpec extends ValidationSuite {
   }
 
   test("a scoped validation can have multible validations for one scope") {
-    val value = new CTest("", "", "")
     import SymbolScope._
-    value.validateScoped('too)(
-      inScope('foo, 'too)(notBlank(value.a) forAttribute 'a),
-      inScope('foo2)(notBlank(value.b) forAttribute 'b),
-      inScope('foo2)(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateScoped('too)(
+      inScope('foo, 'too)(notBlank(emptyVal.a) forAttribute 'a),
+      inScope('foo2)(notBlank(emptyVal.b) forAttribute 'b),
+      inScope('foo2)(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithErrors(
         new IsEmptyStringError(Path.wrap("/a"))
       )
   }
 
   test("a scoped validation can have validate for any of multile scopes") {
-    val value = new CTest("", "", "")
     import SymbolScope._
-    value.validateAnyScope('foo, 'foo2)(
-      inScope('foo)(notBlank(value.a) forAttribute 'a),
-      inScope('foo1)(notBlank(value.b) forAttribute 'b),
-      inScope('foo2)(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateAnyScope('foo, 'foo2)(
+      inScope('foo)(notBlank(emptyVal.a) forAttribute 'a),
+      inScope('foo1)(notBlank(emptyVal.b) forAttribute 'b),
+      inScope('foo2)(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithErrors(
         new IsEmptyStringError(Path.wrap("/a")),
         new IsEmptyStringError(Path.wrap("/c"))
@@ -73,25 +69,23 @@ class ScopeSpec extends ValidationSuite {
   }
 
   test("a scoped validation can have validate for all of multile scopes") {
-    val value = new CTest("", "", "")
     import SymbolScope._
-    value.validateAllScopes('foo, 'foo2)(
-      inScope('foo, 'foo2, 'foo3)(notBlank(value.a) forAttribute 'a),
-      inScope('foo1)(notBlank(value.b) forAttribute 'b),
-      inScope('foo2)(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateAllScopes('foo, 'foo2)(
+      inScope('foo, 'foo2, 'foo3)(notBlank(emptyVal.a) forAttribute 'a),
+      inScope('foo1)(notBlank(emptyVal.b) forAttribute 'b),
+      inScope('foo2)(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithError(
         new IsEmptyStringError(Path.wrap("/a"))
       )
   }
 
   test("a custom scope can be inferred from a scalaz.Equals instance") {
-    val value = new CTest("", "", "")
     import EqualityScope._
     import scalaz.std.string._
-    value.validateScoped("foo2")(
-      inScope("foo")(notBlank(value.a) forAttribute 'a),
-      inScope("foo2")(notBlank(value.b) forAttribute 'b),
-      inScope("foo2")(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateScoped("foo2")(
+      inScope("foo")(notBlank(emptyVal.a) forAttribute 'a),
+      inScope("foo2")(notBlank(emptyVal.b) forAttribute 'b),
+      inScope("foo2")(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithErrors(
         new IsEmptyStringError(Path.wrap("/b")),
         new IsEmptyStringError(Path.wrap("/c"))
@@ -99,13 +93,12 @@ class ScopeSpec extends ValidationSuite {
   }
 
   test("a scoped validation can apply validations in a hierarchical scope") {
-    val value = new CTest("", "", "")
     import Path._
     import PathScope._
-    value.validateScoped("/foo".asPath)(
-      inScope("/foo/a".asPath)(notBlank(value.a) forAttribute 'a),
-      inScope("/foo/b".asPath)(notBlank(value.b) forAttribute 'b),
-      inScope("/bar/c".asPath)(notBlank(value.c) forAttribute 'c)
+    emptyVal.validateScoped("/foo".asPath)(
+      inScope("/foo/a".asPath)(notBlank(emptyVal.a) forAttribute 'a),
+      inScope("/foo/b".asPath)(notBlank(emptyVal.b) forAttribute 'b),
+      inScope("/bar/c".asPath)(notBlank(emptyVal.c) forAttribute 'c)
     ) should beInvalidWithErrors(
         new IsEmptyStringError(Path.wrap("/a")),
         new IsEmptyStringError(Path.wrap("/b"))
@@ -113,9 +106,8 @@ class ScopeSpec extends ValidationSuite {
   }
 
   test("a hierarchical scope treads parent/child scopes as matches") {
-    import PathScope._
     import Path._
-    val hs = implicitly[Scope[PathString]]
+    val hs = PathScope.pathScope
     hs.matches("/foo".asPath, "/foo/bar".asPath) should equal(true)
     hs.matches("/foo/bar".asPath, "/foo".asPath) should equal(false)
     hs.matches("/foo".asPath, "/bar/c".asPath) should equal(false)
